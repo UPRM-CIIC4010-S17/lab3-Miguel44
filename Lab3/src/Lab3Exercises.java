e22ead21f588f6d469dd6c5f23535a05eaa3e6af
micky <micky@LAPTOP-V1UQLLIA>
2017-02-07 21:26:58 +0400

    Size changed, Location is now centered,uncomment line 7

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -6,7 +6,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Hello, world!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocationRelativeTo(null);
-		myFrame.setSize(200, 300);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 		
 	}
