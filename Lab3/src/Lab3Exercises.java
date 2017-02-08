bb0b44d908ead1f4ba69d4244ca67333346ae70f
4015a14 <4015a14@grosella.ece.uprm.edu>
2017-02-08 08:23:40 -0400

    Created new class to fill frame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,9 +4,12 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+        
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		
 	}
